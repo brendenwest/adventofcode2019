e3271b9fe043daf7c7b9efcf98993a377d6fcf16
Brenden West <brenden.west@cbre.com>
2019-12-01 23:03:15 -0800

    day1 done

---
+++ day1/fuel_required.py
@@ -0,0 +1,28 @@
+
+def fuel_required(mass):
+    if mass >= 6:
+        return mass//3-2
+    return 0
+
+def total_fuel(mass):
+    fuel = fuel_required(mass)
+    if fuel == 0:
+        return fuel
+    else:
+        return fuel + total_fuel(fuel)
+
+assert fuel_required(12) == 2
+assert fuel_required(14) == 2
+assert fuel_required(1969) == 654
+assert fuel_required(100756) == 33583
+assert fuel_required(5) == 0
+
+assert total_fuel(1969) == 966
+assert total_fuel(100756) == 50346
+
+inputs = open("input.txt", "r").readlines()
+total = 0
+for mass in inputs:
+    total += total_fuel(int(mass))
+
+print("total fuel",total)
